8d36ca02aa584e9933c904fbe5366d4413f65604
70_10 <7010.dev@gmail.com>
2019-05-26 09:21:38 +0900

    [mod] change mocha to jest

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,12 +1,11 @@
 import { flatten, humanizeDollar } from "../src/util";
-import assert = require("assert");
 
 describe("util", () => {
   it("flatten", () => {
-    assert.deepEqual(flatten([[1,2,4],333, [5,4,6]]) , [1, 2, 4, 333, 5, 4, 6]);
+    expect(flatten([[1, 2, 4], 333, [5, 4, 6]])).toEqual([1, 2, 4, 333, 5, 4, 6]);
   });
 
   it("humanizeDollar", () => {
-    assert(humanizeDollar(10000) === "$10,000.00");
+    expect(humanizeDollar(10000)).toBe("$10,000.00");
   });
 });
